1505d6a50f91c0fae19ffa283e786c02ecaef17b
MoisesACC <moisesaaroncc03@gmail.com>
2025-10-14 23:39:32 -0500

    Cambios...

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Image from "next/image"
 import { registerUser } from "@/lib/auth"
+import { Navbar } from "@/components/navbar"
 
 export default function RegistroPage() {
   const router = useRouter()
